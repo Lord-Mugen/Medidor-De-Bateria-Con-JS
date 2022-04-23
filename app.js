f722cb42eceb1c84303a586f241aaa94c3640d0d
Lord-Mugen <christianjavieruizcortes@gmail.com>
2022-04-23 12:34:14 -0500

    Done

---
+++ app.js
@@ -0,0 +1,6 @@
+const batteryLevel = document.querySelector(".battery");
+navigator.getBattery().then(function (battery){
+    const level = battery.level;
+    const status = level * 100 + "%";
+    batteryLevel.innerHTML = status;
+}); 
